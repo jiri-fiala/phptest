d982fbd9cfcf539dae4b1b4317531e1e3f4e2f79
Jiří Fiala <jfiala@redhat.com>
2016-12-20 10:21:17 +0100

    initial commit

---
+++ headers.php
@@ -0,0 +1,13 @@
+<?php
+
+foreach (getallheaders() as $name => $value) {
+    echo "<b>$name</b>: $value<br />\n";
+}
+
+
+echo "<br /><br /><b>\$_SERVER['HTTP_REFERER']</b>:";
+echo $_SERVER['HTTP_REFERER'];
+
+echo "<br /><br /><b>\$_SERVER:</b><br />";
+print_r($_SERVER);
+
